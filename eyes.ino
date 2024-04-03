015cc5a9112a27be4243472826220aa7760d6572
wfsbear <47589206+ilikecinepol@users.noreply.github.com>
2024-04-03 10:28:48 +0300

    Update eyes.ino

--- eyes.ino
+++ eyes.ino
@@ -1,46 +1,19 @@
 #include <Servo.h>
-#include <AccelStepper.h> // Подключаем библиотеку для шагового двигателя
 
 // Пины для подключения сервомоторов
-const int servoUpDownPinLeftEye = 5;    // пин для серво поворота вверх-вниз левого глаза
-const int servoLeftRightPinLeftEye = 6; // пин для серво поворота влево-вправо левого глаза
-const int servoUpDownPinRightEye = 9;   // пин для серво поворота вверх-вниз правого глаза
+const int servoUpDownPinLeftEye = 5;      // пин для серво поворота вверх-вниз левого глаза
+const int servoLeftRightPinLeftEye = 6;   // пин для серво поворота влево-вправо левого глаза
+const int servoUpDownPinRightEye = 9;     // пин для серво поворота вверх-вниз правого глаза
 const int servoLeftRightPinRightEye = 10; // пин для серво поворота влево-вправо правого глаза
 
-// Пины для подключения шагового двигателя
-const int stepperMotorPins[] = {2, 3, 4, 7}; // IN1, IN2, IN3, IN4
-
-// Пины для подключения джойстика
-const int joyStickXPin = A0; // Пин для оси X джойстика
-const int joyStickYPin = A1; // Пин для оси Y джойстика
-const int joyStickSwPin = 8; // Пин для кнопки джойстика
+const int maxDeltaX = 20;
+const int maxDeltaY = maxDeltaX;
 
 // Объекты для управления сервомоторами
 Servo servoUpDownLeftEye;
 Servo servoLeftRightLeftEye;
 Servo servoUpDownRightEye;
 Servo servoLeftRightRightEye;
-
-// Объект для управления шаговым мотором
-AccelStepper stepperMotor(1, stepperMotorPins[0], stepperMotorPins[1], stepperMotorPins[2], stepperMotorPins[3]); // указываем тип драйвера и пины подключения
-
-// Текущие позиции сервомоторов
-int currentUpDownPosLeftEye = 90;
-int currentLeftRightPosLeftEye = 90;
-int currentUpDownPosRightEye = 90;
-int currentLeftRightPosRightEye = 90;
-
-// Переменные для сглаживания движения глаз
-int previousUpDownPosLeftEye = 90;
-int previousLeftRightPosLeftEye = 90;
-int previousUpDownPosRightEye = 90;
-int previousLeftRightPosRightEye = 90;
-
-// Функция для сглаживания движения глаз
-void smoothEyeMovement(int& currentPos, int& previousPos, int targetPos) {
-  currentPos = (targetPos * 0.02) + (previousPos * 0.98);
-  previousPos = currentPos;
-}
 
 void setup() {
   // Устанавливаем пины сервомоторов как выходы
@@ -55,37 +28,36 @@ void setup() {
   servoUpDownRightEye.attach(servoUpDownPinRightEye);
   servoLeftRightRightEye.attach(servoLeftRightPinRightEye);
 
-  // Устанавливаем пины шагового двигателя как выходы
-  for (int i = 0; i < 4; i++) {
-    pinMode(stepperMotorPins[i], OUTPUT);
-  }
+  servoUpDownLeftEye.write(90);
+  servoLeftRightLeftEye.write(90);
+  servoUpDownRightEye.write(90);
+  servoLeftRightRightEye.write(90);
 
-  // Устанавливаем скорость шагового двигателя
-  stepperMotor.setSpeed(100);
+  delay(100000);
+
+  // Инициализируем Serial порт
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Получаем значения с джойстика
-  int joyStickXValue = analogRead(joyStickXPin);
-  int joyStickYValue = analogRead(joyStickYPin);
+  // Ждем, пока данные придут через Serial порт
+  while (Serial.available() < 7) {
+    delay(10);
+  }
 
-  // Преобразуем значения джойстика в углы для движения глазами
-  int eyeMovementXLeft = map(joyStickXValue, 0, 1023, 175, 15);
-  int eyeMovementYLeft = map(joyStickYValue, 0, 1023, 175, 15);
-  int eyeMovementXRight = map(joyStickXValue, 0, 1023, 175, 15);
-  int eyeMovementYRight = map(joyStickYValue, 0, 1023, 15, 175);
+  // Читаем данные из Serial порта
+  int delta_x = Serial.parseInt();
+  int delta_y = Serial.parseInt();
+  // Считываем разделитель
+  Serial.read(); // Пропускаем запятую
 
-  // Двигаем глазами в зависимости от позиции джойстика
-  smoothEyeMovement(currentUpDownPosLeftEye, previousUpDownPosLeftEye, eyeMovementYLeft);
-  smoothEyeMovement(currentLeftRightPosLeftEye, previousLeftRightPosLeftEye, eyeMovementXLeft);
-  smoothEyeMovement(currentUpDownPosRightEye, previousUpDownPosRightEye, eyeMovementYRight);
-  smoothEyeMovement(currentLeftRightPosRightEye, previousLeftRightPosRightEye, eyeMovementXRight);
-
-  // Поворачиваем сервомоторы в соответствии с новыми позициями
-  servoUpDownLeftEye.write(currentUpDownPosLeftEye);
-  servoLeftRightLeftEye.write(currentLeftRightPosLeftEye);
-  servoUpDownRightEye.write(currentUpDownPosRightEye);
-  servoLeftRightRightEye.write(currentLeftRightPosRightEye);
-
-  // Другие операции управления шаговым двигателем
+  // Переводим значения в углы поворота для сервомоторов
+  int servoAngleX = map(delta_x, -320, 320, (90 + (maxDeltaX / 2)), (90 - (maxDeltaX / 2)));
+  //int servoAngleY = map(delta_y, -240, 240, (90 - (maxDeltaX / 2)), (90 + (maxDeltaX / 2)));
+  int servoAngleY = 90;
+  // Поворачиваем сервомоторы в соответствии с углами
+  servoUpDownLeftEye.write(servoAngleY);
+  servoLeftRightLeftEye.write(servoAngleX);
+  servoUpDownRightEye.write(servoAngleY);
+  servoLeftRightRightEye.write(servoAngleX);
 }
